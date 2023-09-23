e12e96bc40faa90d98ed5db1d0ff769f72fc6624
villi02 <villiarnar1@gmail.com>
2023-09-23 14:49:24 +0200

    revert back to middle height

--- mtads-website/src/app/page.tsx
+++ mtads-website/src/app/page.tsx
@@ -1,16 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
-import Dropdown from "../components/dropdownMenu";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-start w-full flex-1 px-20 text-center mt-10">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-5xl font-bold">
           Velkommen til{" "}
           <a className="text-blue-450">NorwAI for søk av datasett</a>
