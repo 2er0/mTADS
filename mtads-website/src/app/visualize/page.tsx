8d389d937fdee6ba8be397b3314f3454d317c602
villi02 <villiarnar1@gmail.com>
2023-10-05 14:06:19 +0200

    updated with plot names and made improvements overall

--- mtads-website/src/app/visualize/page.tsx
+++ mtads-website/src/app/visualize/page.tsx
@@ -75,78 +75,95 @@ const Page = () => {
     if (d3Container.current) {
       d3.select(d3Container.current).selectAll("*").remove();
       // Set the dimensions and margins of the graph
-      let margin = { top: 20, right: 20, bottom: 30, left: 50 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+      const fileNames = [
+        "train_anomaly.csv",
+        "train_no_anomaly.csv",
+        "test.csv",
+      ];
+      fileNames.forEach((fileName, idx) => {
+        const filePath = `fsb_timeseries/${encodedValue}/${fileName}`;
 
-      // Set the ranges
-      let x = d3.scaleLinear().range([0, width]);
-      let y = d3.scaleLinear().range([height, 0]);
+        let margin = { top: 20, right: 20, bottom: 30, left: 50 },
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
 
-      // Define the line
-      let valueline = d3
-        .line<DataPoint>()
-        .x((d) => x(d.timestamp))
-        .y((d) => y(d["value-0"]));
+        // Set the ranges
+        let x = d3.scaleLinear().range([0, width]);
+        let y = d3.scaleLinear().range([height, 0]);
 
-      let valueline2 = d3
-        .line<DataPoint>()
-        .x((d) => x(d.timestamp))
-        .y((d) => y(d["value-1"]));
+        // Define the line
+        let valueline = d3
+          .line<DataPoint>()
+          .x((d) => x(d.timestamp))
+          .y((d) => y(d["value-0"]));
 
-      let svg = d3
-        .select(d3Container.current)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        let valueline2 = d3
+          .line<DataPoint>()
+          .x((d) => x(d.timestamp))
+          .y((d) => y(d["value-1"]));
 
-        const filePath = `fsb_timeseries/${encodedValue}/train_anomaly.csv`;
-
-      d3.csv(filePath).then((data: d3.DSVRowArray<string>) => {
-        // Explicitly define the structure expected in the CSV for safety
-        const processedData: DataPoint[] = data.map((d: any) => {
-          return {
-            timestamp: +d.timestamp,
-            ...d,
-          };
-        });
-
-        const keys = Object.keys(data[0]).filter((k) => k !== "timestamp");
-
-        x.domain(d3.extent(data, (d) => +d.timestamp) as [number, number]);
-
-        y.domain([
-          0,
-          d3.max(data, (d) => Math.max(...keys.map((key) => +d[key]))) ?? 0,
-        ]);
-
-        const lines = keys.map((key, i) => {
-          return d3
-            .line<DataPoint>()
-            .x((d) => x(d.timestamp))
-            .y((d) => y(d[key as keyof DataPoint]));
-        });
-
-        // Add the lines path.
-        lines.forEach((line, i) => {
-          svg
-            .append("path")
-            .data([processedData])
-            .attr("class", "line")
-            .attr("d", line)
-            .style("stroke", d3.schemeCategory10[i]); // Different color for each line
-        });
-
-        // Add the X Axis
-        svg
+        let svg = d3
+          .select(d3Container.current)
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom + 50) // Add space below each plot
           .append("g")
-          .attr("transform", `translate(0, ${height})`)
-          .call(d3.axisBottom(x));
+          .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-        // Add the Y Axis
-        svg.append("g").call(d3.axisLeft(y));
+        d3.csv(filePath).then((data: d3.DSVRowArray<string>) => {
+          // Explicitly define the structure expected in the CSV for safety
+          const processedData: DataPoint[] = data.map((d: any) => {
+            return {
+              timestamp: +d.timestamp,
+              ...d,
+            };
+          });
+
+          const keys = Object.keys(data[0]).filter((k) => k !== "timestamp");
+
+          x.domain(d3.extent(data, (d) => +d.timestamp) as [number, number]);
+
+          y.domain([
+            0,
+            d3.max(data, (d) => Math.max(...keys.map((key) => +d[key]))) ?? 0,
+          ]);
+
+          const lines = keys.map((key, i) => {
+            return d3
+              .line<DataPoint>()
+              .x((d) => x(d.timestamp))
+              .y((d) => y(d[key as keyof DataPoint]));
+          });
+
+          // Add the lines path.
+          lines.forEach((line, i) => {
+            svg
+              .append("path")
+              .data([processedData])
+              .attr("class", "line")
+              .attr("d", line)
+              .style("stroke", d3.schemeCategory10[i]); // Different color for each line
+          });
+
+          svg
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 0 - margin.top / 2)
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("text-decoration", "underline")
+            .style("fill", "white")
+            .text(fileName);
+
+          // Add the X Axis
+          svg
+            .append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));
+
+          // Add the Y Axis
+          svg.append("g").call(d3.axisLeft(y));
+        });
       });
     }
   }, [d3Container.current, encodedValue]);
@@ -156,13 +173,10 @@ const Page = () => {
       <div className="flex flex-col min-h-screen">
         <Dropdown options={options} />
         <div
-          className="flex justify-center items-center h-screen"
+          className="flex flex-col items-center w-full h-screen"
           ref={d3Container}
         />{" "}
-        {/* This div is where we will append our graph */}
-      </div>
-
-      <div>
+        {/* Flex and column will stack children (SVGs) vertically */}
         <footer className="flex items-center justify-center w-full h-24 border-t">
           <p className="mr-1">Laget av</p>
           <Link href="https://github.com/villi02">
