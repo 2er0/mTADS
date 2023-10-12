a0674b7ecf3be5074591fd508b9fc7ee2b7a24d7
villi02 <villiarnar1@gmail.com>
2023-10-12 15:14:01 +0200

    fixed issue with plotting

--- mtads-website/src/app/visualize/page.tsx
+++ mtads-website/src/app/visualize/page.tsx
@@ -28,7 +28,7 @@ const Page = () => {
         "train_no_anomaly.csv",
         "test.csv",
       ];
-      fileNames.forEach((fileName, idx) => {
+      fileNames.forEach((fileName, i) => {
         const filePath = `fsb_timeseries/${encodedValue}/${fileName}`;
 
         let margin = { top: 20, right: 20, bottom: 30, left: 50 },
@@ -47,79 +47,88 @@ const Page = () => {
           .append("g")
           .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-        d3.csv(filePath).then((data: d3.DSVRowArray<string>) => {
-          // Explicitly define the structure expected in the CSV for safety
-          const processedData: DataPoint[] = data.map((d: any) => {
-            return {
-              timestamp: +d.timestamp,
-              ...d,
-            };
-          });
+        d3.csv(filePath)
+          .then((data: d3.DSVRowArray<string>) => {
+            // Explicitly define the structure expected in the CSV for safety
+            const processedData: DataPoint[] = data.map(
+              (d: d3.DSVRowString<string>) => {
+                return {
+                  timestamp: +d.timestamp,
+                  ...d,
+                } as DataPoint;
+              }
+            );
 
-          const keys = Object.keys(data[0]).filter((k) => k !== "timestamp");
+            const keys = Object.keys(data[0]).filter((k) => k !== "timestamp");
 
-          x.domain(d3.extent(data, (d) => +d.timestamp) as [number, number]);
+            x.domain([0, data.length - 1]);
 
-          const allYValues = data.reduce((accum: number[], curr) => {
-            keys.forEach((key) => accum.push(+curr[key]));
-            return accum;
-          }, []);
+            const allYValues = data.reduce((accum: number[], curr) => {
+              keys.forEach((key) => accum.push(+curr[key]));
+              return accum;
+            }, []);
 
-          const minY = Math.min(...allYValues);
-          const maxY = Math.max(...allYValues);
+            const minY = Math.min(...allYValues);
+            const maxY = Math.max(...allYValues);
 
-          // Dynamically adjust height if necessary.
-          // For example, if there is a negative minY value, provide more space. Adjust as per your visualization needs.
-          if (minY < 0) {
-            height = height - minY;
-          }
+            // Dynamically adjust height if necessary.
+            // For example, if there is a negative minY value, provide more space. Adjust as per your visualization needs.
+            if (minY < 0) {
+              height = height - minY;
+            }
 
-          y.domain([minY, maxY]);
+            y.domain([minY, maxY]);
 
-          const lines = keys.map((key, i) => {
-            return d3
-              .line<DataPoint>()
-              .x((d) => x(d.timestamp))
-              .y((d) => y(d[key as keyof DataPoint]));
-          });
-
-          // Add the lines path.
-          lines.forEach((line, i) => {
             svg
-              .append("path")
-              .data([processedData])
-              .attr("class", "line")
-              .attr("d", line)
-              .style("stroke", d3.schemeCategory10[i]); // Different color for each line
+              .append("text")
+              .attr("x", width / 2)
+              .attr("y", 0 - margin.top / 2)
+              .attr("text-anchor", "middle")
+              .style("font-size", "16px")
+              .style("text-decoration", "underline")
+              .style("fill", "white")
+              .text(fileName);
+
+            // Add the X Axis
+            svg
+              .append("g")
+              .attr("transform", `translate(0, ${height})`)
+              .call(d3.axisBottom(x))
+              .attr("class", "axis x-axis")
+              .selectAll("text")
+              .style("fill", "white");
+
+            // Add the Y Axis
+            svg
+              .append("g")
+              .call(d3.axisLeft(y))
+              .attr("class", "axis y-axis")
+              .selectAll("text")
+              .style("fill", "white");
+
+            const lines = keys.map((key, i) => {
+              return d3
+                .line<DataPoint>()
+                .x((d, i) => x(i)) // Use the index i instead of d.timestamp
+                .y((d) => y(d[key as keyof DataPoint]));
+            });
+
+            // Add the lines path.
+            lines.forEach((line, i) => {
+              const pathData = line(processedData);
+              svg
+                .append("path")
+                .data([processedData])
+                .attr("class", "line")
+                .attr("d", pathData)
+                .style("stroke", d3.schemeCategory10[i])
+                .style("fill", "none")
+                .style("stroke-opacity", 0.5); // Add some transparency to see overlapping lines
+            });
+          })
+          .catch((error) => {
+            console.error("Error loading data:", error);
           });
-
-          svg
-            .append("text")
-            .attr("x", width / 2)
-            .attr("y", 0 - margin.top / 2)
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .style("text-decoration", "underline")
-            .style("fill", "white")
-            .text(fileName);
-
-          // Add the X Axis
-          svg
-            .append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(d3.axisBottom(x))
-            .attr("class", "axis x-axis")
-            .selectAll("text")
-            .style("fill", "white");
-
-          // Add the Y Axis
-          svg
-            .append("g")
-            .call(d3.axisLeft(y))
-            .attr("class", "axis y-axis")
-            .selectAll("text")
-            .style("fill", "white");
-        });
       });
     }
   }, [d3Container.current, encodedValue]);
