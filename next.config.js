146e1bad02be2a0bb3250c028d2740c146fe2b2f
2er0 <2er0@dbaumi.at>
2023-10-16 13:52:02 +0200

    fixing next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+
+    output: "export",
+
+    // Add basePath
+    basePath: '/mTADS',
+}
 
 module.exports = nextConfig
